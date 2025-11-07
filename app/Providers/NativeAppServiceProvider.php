cf9e28c4cc2fdeb7cedf35b1b6261f3ef854f5be
Yurei21 <larksabangan@gmail.com>
2025-11-08 00:50:22 +0800

    removed tauri. Moved to nativephp

---
+++ app/Providers/NativeAppServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use Native\Laravel\Facades\Window;
+use Native\Laravel\Contracts\ProvidesPhpIni;
+
+class NativeAppServiceProvider implements ProvidesPhpIni
+{
+    /**
+     * Executed once the native application has been booted.
+     * Use this method to open windows, register global shortcuts, etc.
+     */
+    public function boot(): void
+    {
+        Window::open()
+        ->title('My Financial Tracker')
+        ->width(1200)
+        ->height(800)
+        ->resizable();
+    }
+
+    /**
+     * Return an array of php.ini directives to be set.
+     */
+    public function phpIni(): array
+    {
+        return [
+        ];
+    }
+}
