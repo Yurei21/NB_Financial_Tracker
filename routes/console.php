9a72c68f14079fa3c31541763c0d8940babd2e5f
Yurei <69757457+Yurei21@users.noreply.github.com>
2025-11-25 12:07:09 +0800

    console routing

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,16 @@
 <?php
 
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+$scheduleClosure = require base_path('app/Schedulers/ImportScheduler.php');
+
+app()->booted(function () use ($scheduleClosure) {
+    $scheduleClosure(app(Schedule::class));
+});
